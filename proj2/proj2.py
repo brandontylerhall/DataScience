2ca90e5ed172fc7c1e968ef9c67a15d2a7324bcb
Brandon Hall <bran.tyler.hall@gmail.com>
2025-12-16 15:53:23 -0500

    ordered bars for clarity (except the final one. couldnt figure that one out)

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -1,58 +1,99 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import viridis
 
 sales = pd.read_csv('supermarket_sales.csv')
 
 # Question 1: Which City brings in the most gross income
+city_sales_order = (
+    sales.groupby("City")["gross income"]
+    .sum()
+    .sort_values(ascending=False)
+    .index
+)
+
 q1 = sns.catplot(
     data=sales,
     kind='bar',
     x="City",
     y="gross income",
     estimator=sum,
-    hue="City"
+    hue="City",
+    palette='viridis',
+    order=city_sales_order
 )
 q1.figure.suptitle("Gross Income by City")
 
 # Question 2: Which Payment method is most popular
+payment_order = (
+    sales["Payment"]
+    .value_counts()
+    .index
+)
 q2 = sns.catplot(
     data=sales,
     kind='count',
     x="Payment",
-    hue='Payment'
+    hue='Payment',
+    palette='viridis',
+    order=payment_order
 )
 q2.figure.suptitle("Total Sales By Payment Method")
 
 # Question 3: Which Product line gets the best Ratings
+prod_line_rating_order = (
+    sales.groupby("Product line")["Rating"]
+    .sum()
+    .sort_values(ascending=False)
+    .index
+)
 q3 = sns.catplot(
     data=sales,
     kind="bar",
     x='Rating',
     y='Product line',
-    hue='Product line'
+    hue='Product line',
+    palette='viridis',
+    order=prod_line_rating_order
 )
 q3.figure.suptitle("Highest Avg Rating By Product Line")
 q3.figure.subplots_adjust(top=0.9)
 
 # Question 4: Who spends more on average: Members or Normies
+average_sales_order = (
+    sales.groupby("Customer type")["Total"]
+    .sum()
+    .sort_values(ascending=False)
+    .index
+)
 q4 = sns.catplot(
     data=sales,
     kind="bar",
     x="Customer type",
     y="Total",
-    hue="Customer type"
+    hue="Customer type",
+    palette='viridis',
+    order=average_sales_order
 )
 q4.figure.suptitle("Average Sale Total By Customer Type")
 
 # Question 5: Which Product line makes the most gross income
+prod_line_gross_order = (
+    sales.groupby("Product line")["gross income"]
+    .sum()
+    .sort_values(ascending=False)
+    .index
+)
 q5 = sns.catplot(
     data=sales,
     kind="bar",
     x="gross income",
     y="Product line",
     estimator=sum,
-    hue="Product line"
+    hue="Product line",
+    palette='viridis',
+    order=prod_line_gross_order
 )
 q5.figure.suptitle("Gross Income by Product Line")
 q5.figure.subplots_adjust(top=0.9)
@@ -64,8 +105,9 @@ q6 = sns.catplot(
     x="Total",
     y="Product line",
     col="Customer type",
-    hue="Product line",
-    errorbar=None
+    hue="Gender",
+    errorbar=None,
+    palette='viridis'
 )
 q6.figure.suptitle("Total Sales By Customer Type and Product Line", y=1)
 q6.figure.subplots_adjust(top=0.9)
