7d640a75a8688be45dc61b78619cd75c06f5568f
Brandon Hall <bran.tyler.hall@gmail.com>
2025-12-16 14:56:53 -0500

    init questions to be answered

---
+++ proj2/proj2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sales = pd.read_csv('supermarket_sales.csv')
+
+# Question 1: Which City brings in the most gross income
+
+# Question 2: Which Payment method is most popular
+
+# Question 3: Which Product line gets the best Ratings
+
+# Question 4: Who spends more on average: Members or Normies
+
+# Question 5: Which Product line makes the most gross income
+
+# Question 6: Who (Member vs Normal) buys What (Product) and How Much (Total)
