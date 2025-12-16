1a5fee58cc6b8366c9952e08a488b513ba590402
Brandon Hall <bran.tyler.hall@gmail.com>
2025-12-16 15:23:14 -0500

    added titles to charts and improved readability further

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -13,6 +13,7 @@ q1 = sns.catplot(
     estimator=sum,
     hue="City"
 )
+q1.figure.suptitle("Gross Income by City")
 
 # Question 2: Which Payment method is most popular
 q2 = sns.catplot(
@@ -21,6 +22,7 @@ q2 = sns.catplot(
     x="Payment",
     hue='Payment'
 )
+q2.figure.suptitle("Total Sales By Payment Method")
 
 # Question 3: Which Product line gets the best Ratings
 q3 = sns.catplot(
@@ -30,6 +32,8 @@ q3 = sns.catplot(
     y='Product line',
     hue='Product line'
 )
+q3.figure.suptitle("Highest Avg Rating By Product Line")
+q3.figure.subplots_adjust(top=0.9)
 
 # Question 4: Who spends more on average: Members or Normies
 q4 = sns.catplot(
@@ -39,6 +43,7 @@ q4 = sns.catplot(
     y="Total",
     hue="Customer type"
 )
+q4.figure.suptitle("Average Sale Total By Customer Type")
 
 # Question 5: Which Product line makes the most gross income
 q5 = sns.catplot(
@@ -49,6 +54,9 @@ q5 = sns.catplot(
     estimator=sum,
     hue="Product line"
 )
+q5.figure.suptitle("Gross Income by Product Line")
+q5.figure.subplots_adjust(top=0.9)
+plt.show()
 
 # Question 6: Who (Member vs Normal) buys What (Product) and How Much (Total)
 q6 = sns.catplot(
@@ -60,5 +68,6 @@ q6 = sns.catplot(
     hue="Product line",
     errorbar=None
 )
+q6.figure.suptitle("Total Sales By Customer Type and Product Line", y=1)
+q6.figure.subplots_adjust(top=0.9)
 
-plt.show()
