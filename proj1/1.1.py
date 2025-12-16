d7673263e2fea8795d2ba5fad84956ca464e063e
Brandon Hall <bran.tyler.hall@gmail.com>
2025-12-16 09:17:15 -0500

    created heatmap of data to visualize where data is missing/present

--- proj1/1.1.py
+++ proj1/1.1.py
@@ -1,4 +1,12 @@
+# "excel for python"
 import pandas as pd
+# visualization library
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 titanic = sns.load_dataset('titanic')
+
+print(titanic.info())
+# titanic.isnull() asks every single cell in the csv if it's empty
+sns.heatmap(titanic.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+plt.show()
